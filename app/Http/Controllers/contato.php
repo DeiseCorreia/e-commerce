10afe497a103bad6e84f05489bbb488931ab6dad
DeiseCorreia <71478997+DeiseCorreia@users.noreply.github.com>
2021-05-18 22:08:01 -0300

    update contato.php

--- app/Http/Controllers/contato.php
+++ app/Http/Controllers/contato.php
@@ -1,10 +1,10 @@
-<?php include "inicio.blade.php"
-    $txtnome ="";
-    $txtemail="";
-    if(isset($_POST['nome']) && isset($_POST['email'])){
-       $txtnome =$_POST['nome'];
-        $txtemail=$_POST['email'];
-    }
+<!--<?php include "inicio.blade.php"
+   // $txtnome ="";
+   // $txtemail="";
+   // if(isset($_POST['nome']) && isset($_POST['email'])){
+       //$txtnome =$_POST['nome'];
+      //  $txtemail=$_POST['email'];
+   // }
     
 
-?>
+?>-->
