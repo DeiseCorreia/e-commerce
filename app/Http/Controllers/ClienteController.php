27ea057cff7925c5210d141f57139d21a36992e2
DeiseCorreia <71478997+DeiseCorreia@users.noreply.github.com>
2021-05-19 21:10:21 -0300

    push projeto_criado classes controllers

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//class ClienteController extends Controller
+//{
+    //
+   // public function cadastro(Request $request){
+  //      //echo "Pagina Produto";
+     //   $dta=[];
+//
+     //   return view("cadastro",$dta);
+   /// }
+//}
---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//class ProdutoController extends Controller
+//{
+    //
+   // public function index(Request $request){
+        //echo "Pagina Produto";
+       // $dta=[];
+
+      //  return view("home",$dta);
+    //}
+
+    //public function categoria(Request $request){
+        //echo "categoria;
+      //  $dta=[];
+
+      //  return view("categoria",$dta);
+    //}
+//}
